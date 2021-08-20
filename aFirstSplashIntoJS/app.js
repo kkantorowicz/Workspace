a68280023e7119c4966861bee4c99fcbe899586a
Kayne <kkantorowicz@captechconsulting.com>
2021-08-20 10:34:24 -0500

    add inital structure for game including boot, and variables behind the scenes

--- aFirstSplashIntoJS/app.js
+++ aFirstSplashIntoJS/app.js
@@ -1,1 +1,22 @@
 console.log("First Splash script now logging... ");
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+guesses.textContent = `${guessCount}`;
+lastResult.textContent = 'Wrong!';
+lowOrHi.textContent = 'Low';
+
+console.log(`Random Number: ${randomNumber}`);
+console.log(`NUmber of Guesses: ${guessCount}`);
+console.log(`Reset?: ${resetButton}`);
+
